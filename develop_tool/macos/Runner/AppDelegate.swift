5caa14733f09463b6f668c781c23e29aed3c1efe
yangjie <617958070@qq.com>
2025-04-23 14:36:58 +0800

    mod: 设置窗口大小

--- develop_tool/macos/Runner/AppDelegate.swift
+++ develop_tool/macos/Runner/AppDelegate.swift
@@ -3,7 +3,15 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+    override func applicationDidFinishLaunching(_ notification: Notification) {
+        if let window = NSApplication.shared.windows.first {
+            window.setContentSize(NSSize(width: 1000, height: 800)) // 设置窗口大小
+            window.minSize = NSSize(width: 800, height: 600)        // 最小尺寸（可选）
+        }
+        super.applicationDidFinishLaunching(notification)
+    }
 }
